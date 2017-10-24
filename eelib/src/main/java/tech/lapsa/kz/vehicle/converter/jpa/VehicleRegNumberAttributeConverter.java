6393f0b6fc53e147652d7c9096704a77fcac17ad
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-24 17:59:06 +0600

    vehicle reg number jpa converter

---
+++ eelib/src/main/java/tech/lapsa/kz/vehicle/converter/jpa/VehicleRegNumberAttributeConverter.java
@@ -0,0 +1,21 @@
+package tech.lapsa.kz.vehicle.converter.jpa;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import tech.lapsa.kz.vehicle.VehicleRegNumber;
+
+@Converter(autoApply = true)
+public class VehicleRegNumberAttributeConverter implements AttributeConverter<VehicleRegNumber, String> {
+
+    @Override
+    public String convertToDatabaseColumn(VehicleRegNumber attribute) {
+	return attribute == null ? null : attribute.getNumber();
+    }
+
+    @Override
+    public VehicleRegNumber convertToEntityAttribute(String dbData) {
+	return dbData == null ? null : VehicleRegNumber.of(dbData);
+    }
+
+}
