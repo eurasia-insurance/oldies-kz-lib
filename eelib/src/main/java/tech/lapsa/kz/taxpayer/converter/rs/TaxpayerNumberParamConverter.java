51e2668ae92653230695822a04b61e31cda475ca
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-26 14:45:57 +0600

    used rs-commons ParamConverter

--- eelib/src/main/java/tech/lapsa/kz/taxpayer/converter/rs/TaxpayerNumberParamConverter.java
+++ eelib/src/main/java/tech/lapsa/kz/taxpayer/converter/rs/TaxpayerNumberParamConverter.java
@@ -1,11 +1,16 @@
 package tech.lapsa.kz.taxpayer.converter.rs;
 
+import java.util.Map.Entry;
+
 import javax.ws.rs.ext.ParamConverter;
 
+import tech.lapsa.java.commons.function.MyMaps;
 import tech.lapsa.java.commons.function.MyObjects;
+import tech.lapsa.javax.rs.params.MyParamConverterProvider;
 import tech.lapsa.kz.taxpayer.TaxpayerNumber;
 
-public class TaxpayerNumberParamConverter implements ParamConverter<TaxpayerNumber> {
+public class TaxpayerNumberParamConverter
+	implements ParamConverter<TaxpayerNumber>, MyParamConverterProvider<TaxpayerNumber> {
 
     @Override
     public TaxpayerNumber fromString(String value) {
@@ -17,4 +22,11 @@ public class TaxpayerNumberParamConverter implements ParamConverter<TaxpayerNumb
 	return MyObjects.requireNonNull(value, "value").getNumber();
     }
 
+    private static Entry<Class<TaxpayerNumber>, ParamConverter<TaxpayerNumber>> ENTRY = MyMaps
+	    .entry(TaxpayerNumber.class, new TaxpayerNumberParamConverter());
+
+    @Override
+    public Entry<Class<TaxpayerNumber>, ParamConverter<TaxpayerNumber>> entry() {
+	return ENTRY;
+    }
 }
--- eelib/src/main/java/tech/lapsa/kz/vehicle/converter/rs/VehicleRegNumberParamConverter.java
+++ eelib/src/main/java/tech/lapsa/kz/vehicle/converter/rs/VehicleRegNumberParamConverter.java
@@ -1,11 +1,16 @@
 package tech.lapsa.kz.vehicle.converter.rs;
 
+import java.util.Map.Entry;
+
 import javax.ws.rs.ext.ParamConverter;
 
+import tech.lapsa.java.commons.function.MyMaps;
 import tech.lapsa.java.commons.function.MyObjects;
+import tech.lapsa.javax.rs.params.MyParamConverterProvider;
 import tech.lapsa.kz.vehicle.VehicleRegNumber;
 
-public class VehicleRegNumberParamConverter implements ParamConverter<VehicleRegNumber> {
+public class VehicleRegNumberParamConverter
+	implements ParamConverter<VehicleRegNumber>, MyParamConverterProvider<VehicleRegNumber> {
 
     @Override
     public VehicleRegNumber fromString(String v) {
@@ -17,4 +22,11 @@ public class VehicleRegNumberParamConverter implements ParamConverter<VehicleReg
 	return MyObjects.requireNonNull(value, "value").getNumber();
     }
 
+    private static Entry<Class<VehicleRegNumber>, ParamConverter<VehicleRegNumber>> ENTRY = MyMaps
+	    .entry(VehicleRegNumber.class, new VehicleRegNumberParamConverter());
+
+    @Override
+    public Entry<Class<VehicleRegNumber>, ParamConverter<VehicleRegNumber>> entry() {
+	return ENTRY;
+    }
 }
