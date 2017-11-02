507e176b7d1c2d14c166d151e2e062883f6db5fa
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-02 18:23:00 +0600

    fix

--- core/src/main/java/tech/lapsa/kz/vehicle/converter/jaxb/XmlVehicleRegNumberAdapter.java
+++ core/src/main/java/tech/lapsa/kz/vehicle/converter/jaxb/XmlVehicleRegNumberAdapter.java
@@ -3,18 +3,23 @@ package tech.lapsa.kz.vehicle.converter.jaxb;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import tech.lapsa.java.commons.function.MyObjects;
+import tech.lapsa.java.commons.function.MyStrings;
 import tech.lapsa.kz.vehicle.VehicleRegNumber;
 
 public class XmlVehicleRegNumberAdapter extends XmlAdapter<String, VehicleRegNumber> {
 
     @Override
     public VehicleRegNumber unmarshal(String v) throws Exception {
-	return VehicleRegNumber.of(v);
+	return MyStrings.empty(v) //
+		? null //
+		: VehicleRegNumber.of(v);
     }
 
     @Override
     public String marshal(VehicleRegNumber v) throws Exception {
-	return MyObjects.requireNonNull(v, "vehicleRegNumber").getNumber();
+	return MyObjects.isNull(v) //
+		? null //
+		: v.getNumber();
     }
 
 }
